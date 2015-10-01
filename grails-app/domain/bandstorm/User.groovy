a0ea256d3fa6be34f6117bc766ac16fb9e57792e
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-10-01 11:24:21 +0200

    Merge PR #24 - Models creation

--- grails-app/domain/bandstorm/User.groovy
+++ grails-app/domain/bandstorm/User.groovy
@@ -7,16 +7,27 @@ package bandstorm
  */
 class User {
 
-    String username;
-    String email;
-    String firstName;
-    String LastName;
-    String birthDate;
-    String country;
-    Date dateCreated;
+    String username
+    String email
+    String firstName
+    String lastName
+    Date birthDate
+    String country
+    Date dateCreated
 
-    static hasMany = [posts : Status, interests : Tag, participates : Event, mangages : Event, groupsFollowed : Group ]
+    static hasMany = [posts : Status, interests : Tag, participates : Event, manages : Event, groupsFollowed : Group ]
 
     static constraints = {
+        username blank: false, nullable: false, minSize: 3
+        email email : true, blank : false
+        firstName blank: false, nullable: false
+        lastName blank: false, nullable: false
+        country blank: false, nullable: false
+        birthDate max: new Date()
+        posts nullable: true
+        interests nullable: true
+        participates nullable: true
+        manages nullable: true
+        groupsFollowed nullable: true
     }
 }
--- test/unit/bandstorm/UserSpec.groovy
+++ test/unit/bandstorm/UserSpec.groovy
@@ -9,12 +9,100 @@ import spock.lang.Specification
 @TestFor(User)
 class UserSpec extends Specification {
 
-    def setup() {
+    void "is Username valid"() {
+
+        when: "we create a user instance"
+        User user = new User(username: userN, email: "test@mydomain.com",
+                firstName: "jon", lastName: "doe", birthDate: new Date(), country: "somewhere")
+
+        then: "validation..."
+        user.validate() == usernameOK
+
+        where: "username and usernameOK"
+        userN  | usernameOK
+        "test" | true
+        "tt"   | false
+        ""     | false
+        null   | false
     }
 
-    def cleanup() {
+    void "is email valid"() {
+
+        when: "we create a user instance"
+        User user = new User(username: "userName", email: emailAd,
+                firstName: "jon", lastName: "doe", birthDate: new Date(), country: "somewhere")
+
+        then: "validation..."
+        user.validate() == emailOK
+
+        where: "emailAd and emailOK"
+        emailAd             | emailOK
+        "test@mydomain.com" | true
+        "test"              | false
+        ""                  | false
+        null                | false
     }
 
-    void "test something"() {
+    void "is fistNameValid"() {
+
+        when: "we create a user instance"
+        User user = new User(username: "userName", email: "test@mydomain.com",
+                firstName: fstName, lastName: "doe", birthDate: new Date(), country: "somewhere")
+        then: "validation..."
+        user.validate() == fstNameOK
+
+        where: "fstName and fstNameOK"
+        fstName         | fstNameOK
+        "a name"        | true
+        "test"          | true
+        ""              | false
+        null            | false
+    }
+
+    void "is Last Name Valid"() {
+
+        when: "we create a user instance"
+        User user = new User(username: "userName", email: "test@mydomain.com",
+                firstName: "jon", lastName: lstName, birthDate: new Date(), country: "somewhere")
+        then: "validation..."
+        user.validate() == lstNameOK
+
+        where: "lstName and lstNameOK"
+        lstName         | lstNameOK
+        "a name"        | true
+        "test"          | true
+        ""              | false
+        null            | false
+    }
+
+    void "is country Valid"() {
+
+        when: "we create a user instance"
+        User user = new User(username: "userName", email: "test@mydomain.com",
+                firstName: "jon", lastName: "doe", birthDate: new Date(), country: cntry)
+        then: "validation..."
+        user.validate() == cntryOK
+
+        where: "cntry and cntryOK"
+        cntry           | cntryOK
+        "a name"        | true
+        "test"          | true
+        ""              | false
+        null            | false
+    }
+
+    void "is birthDate validate"() {
+        when: "we create a user instance"
+        User user = new User(username: "userName", email: "test@mydomain.com",
+                firstName: "jon", lastName: "doe", birthDate: date, country: "Country")
+        then: "validation..."
+        user.validate() == dateOK
+
+        where: "date and dateOK"
+        date                                                            | dateOK
+        new Date()                                                      | true
+        Date.parse("yyyy-MM-dd hh:mm:ss", "2014-04-03 1:23:45")         | true
+        Date.parse("yyyy-MM-dd hh:mm:ss", "2016-04-03 1:23:45")         | false
+        null                                                            | false
     }
 }
