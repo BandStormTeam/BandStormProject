98b77af69e54dee40a7bbc46cdb64a533eaace40
johnSilver7 <inspiredevelopment@hotmail.fr>
2015-10-01 11:02:19 +0200

    Add javadoc on groups. #3

--- grails-app/domain/bandstorm/Group.groovy
+++ grails-app/domain/bandstorm/Group.groovy
@@ -1,13 +1,20 @@
 package bandstorm
 
+/**
+ * Represent a group of people (users and external real persons) in the scope of a band.
+ * In the future this class could also represent a musical association from a town for instance.
+ * */
+
 class Group {
     String name
     Date dateCreated
     String description
+    String address
 
     static constraints = {
         name blank:false
         dateCreated date: true
         description blank: false
+        address nullable: true, blank: true
     }
 }
