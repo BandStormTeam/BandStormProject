129ea223915dd3e4691c6cb53de764128b4aec54
vlaks <roletto.dylan@gmail.com>
2015-10-01 10:25:01 +0200

    unit test for GroupMember domain class #3

--- test/unit/bandstorm/GroupMemberSpec.groovy
+++ test/unit/bandstorm/GroupMemberSpec.groovy
@@ -9,12 +9,27 @@ import spock.lang.Specification
 @TestFor(GroupMember)
 class GroupMemberSpec extends Specification {
 
-    def setup() {
+    void "test a validate GroupMember creation"() {
+        when: "a new valid GroupMember is created"
+        def user = Mock(User)
+        def group = Mock(Group)
+        Date date = new Date()
+        GroupMember groupMember = new GroupMember(dateCreated: date,user: user,group: group)
+
+        then: "the groupMember is valid"
+        groupMember.validate()
     }
 
-    def cleanup() {
-    }
+    void "test an unvalidate GroupMember creation"(){
+        when: "a new unvalidate Group Member is created"
+        Date date = new Date()
+        GroupMember groupMember = new GroupMember(dateCreated: date,user: user,group: group)
 
-    void "test something"() {
+        then: "the groupMember is invalidate"
+        !groupMember.validate()
+
+        where: user       | group
+               Mock(User) | null
+               null       | Mock(Group)
     }
 }
