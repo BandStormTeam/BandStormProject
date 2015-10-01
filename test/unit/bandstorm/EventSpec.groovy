e2b1edc4c5563a4067488e0f6400c9bd6205c761
johnSilver7 <inspiredevelopment@hotmail.fr>
2015-10-01 09:56:34 +0200

    #3 Ajout du test unitaire de la classe Event

--- test/unit/bandstorm/EventSpec.groovy
+++ test/unit/bandstorm/EventSpec.groovy
@@ -15,6 +15,46 @@ class EventSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test a valid event"() {
+        given: "a event correctly created"
+        Event event = new Event()
+        event.name = "Festival des charrues volantes"
+        event.address = "8 Avenue des Colombes 31400 Toulouse"
+        event.description = "Festival organisé sur le thème de la musique péruvienne à travers les mouvements musicaux"
+
+        when: ""
+        def res = event.validate()
+
+        then:""
+        res == true
+
+        and: "the event has no errors"
+        !event.hasErrors()
+
+    }
+
+    void "test invalid event"() {
+        given: "an event"
+        Event event = new Event()
+        event.name = aName
+        event.description = aDescription
+        event.address = anAddress
+
+
+        when: "validating an event"
+        def isValid = event.validate()
+
+        then: "the event is not valid"
+        isValid == false
+
+        where:
+        aName |  anAddress | aDescription
+        "" | "8 Avenue des Colombes 31400 Toulouse" | "Festival organisé sur le thème de la musique péruvienne à travers les mouvements musicaux"
+        "Festival des charrues volantes Festival des charrues volantes Festival des charrues volantes" | "8 Avenue des Colombes 31400 Toulouse" | "Festival organisé sur le thème de la musique péruvienne à travers les mouvements musicaux"
+        "Fe" | "8 Avenue des Colombes 31400 Toulouse" | "Festival organisé sur le thème de la musique péruvienne à travers les mouvements musicaux"
+        "Festival des charrues volantes" | "8 Avenue des Colombes 31400 Toulouse 8 Avenue des Colombes 31400 Toulouse 8 Avenue des Colombes 31400 Toulouse 8 Avenue des Colombes 31400 Toulouse 8 Avenue des Colombes 31400 Toulouse 8 Avenue des Colombes 31400 Toulouse" | "Festival organisé sur le thème de la musique péruvienne à travers les mouvements musicaux"
+        "Festival des charrues volantes" | "ab" | "Festival organisé sur le thème de la musique péruvienne à travers les mouvements musicaux"
+        "Festival des charrues volantes" | "8 Avenue des Colombes 31400 Toulouse" | "F"
+
     }
 }
