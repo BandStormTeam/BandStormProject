e48441bdb5a03ebec1d3baf423fab2602769bcf6
johnSilver7 <inspiredevelopment@hotmail.fr>
2015-10-01 09:51:42 +0200

    Modification de la classe de modèle Event pour représenter le lien avec les tags. Ajout des contraintes. #3

--- grails-app/domain/bandstorm/Event.groovy
+++ grails-app/domain/bandstorm/Event.groovy
@@ -2,11 +2,17 @@ package bandstorm
 
 class Event {
 
-    String name;
-    Date dateCreated;
-    String address;
-    String description;
+    String name
+    Date dateCreated
+    String address
+    String description
+
+    static hasMany = [tags : Tag]
 
     static constraints = {
+        name blank: false, maxSize: 35, minSize: 3
+        address maxSize: 200, minSize: 10
+        description minSize: 2
+        tags nullable: true
     }
 }
