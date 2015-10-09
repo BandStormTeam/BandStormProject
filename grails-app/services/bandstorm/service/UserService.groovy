9fbb9bcc3b7fdb2a6eb66ace2e2b10da85571f72
johnSilver7 <inspiredevelopment@hotmail.fr>
2015-10-09 13:23:46 +0200

    Update user service with addStatusToUser method #39

--- grails-app/services/bandstorm/service/UserService.groovy
+++ grails-app/services/bandstorm/service/UserService.groovy
@@ -1,5 +1,9 @@
 package bandstorm.service
 
+import bandstorm.Status
+import bandstorm.User
+import bandstorm.dao.StatusDaoService
+import bandstorm.dao.UserDAOService
 import grails.transaction.Transactional
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
@@ -10,6 +14,8 @@ import org.springframework.security.core.context.SecurityContextHolder
 @Transactional
 class UserService {
     AuthenticationManager authenticationManager
+    StatusDaoService statusDaoService
+    UserDAOService userDAOService
     def logoutHandlers
 
     def logIn(String username, String password) throws AuthenticationException {
@@ -21,9 +27,15 @@ class UserService {
     def logout(request, response) {
         Authentication auth = SecurityContextHolder.context.authentication
         if (auth) {
-            logoutHandlers.each  { handler->
-                handler.logout(request,response,auth)
+            logoutHandlers.each { handler ->
+                handler.logout(request, response, auth)
             }
         }
+    }
+
+    def addStatusToUser(User user, Status status) {
+        user.addToPosts(status)
+        statusDaoService.create(status)
+        userDAOService.update(user)
     }
 }
