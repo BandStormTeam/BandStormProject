fc4eb5d6c5918406923f8b0206e0cf92e4097a71
vlaks <roletto.dylan@gmail.com>
2015-10-12 09:14:12 +0200

    change on status service integration test #23

--- test/integration/bandstorm/StatusServiceIntegrationSpec.groovy
+++ test/integration/bandstorm/StatusServiceIntegrationSpec.groovy
@@ -12,16 +12,16 @@ class StatusServiceIntegrationSpec extends Specification {
 
     void "test getStatusforTimeline method"() {
 
-        given: "2 status"
-        def status1 = new Status(content: "My status 1", lightCount: 0).save()
-        def status2 = new Status(url: "http://www.google.fr", content: "My status 2", lightCount: 0).save()
+        given: "2 status and 10 status from bootstrap"
+        def status1 = new Status(content: "My status test 1", lightCount: 0).save()
+        def status2 = new Status(url: "http://www.google.fr", content: "My status test 2", lightCount: 0).save()
 
         when: "I want to get all status for timeline"
         List<Status> statusList = statusService.getStatusForTimeline()
         println statusList.toString()
 
         then: "we get all status"
-        statusList.size() == 2
+        statusList.size() == 12
 
     }
 }
