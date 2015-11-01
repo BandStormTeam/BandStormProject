3dc2ebf9dbe538e5a9713c424732409e1814dea8
smagras <magras.steve@gmail.com>
2015-11-01 21:16:32 +0100

    comments for tagcontroller #69

--- grails-app/controllers/bandstorm/TagController.groovy
+++ grails-app/controllers/bandstorm/TagController.groovy
@@ -4,24 +4,46 @@ package bandstorm
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
+/**
+ * Controller for tags
+ */
 @Transactional(readOnly = true)
 class TagController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
+    /**
+     * Show the list of tags
+     * @param max: max number of tags to show
+     * @return the list of tags
+     */
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond Tag.list(params), model: [tagInstanceCount: Tag.count()]
     }
 
+    /**
+     * Show details for the tag
+     * @param tagInstance : tag instance
+     * @return details for the tag
+     */
     def show(Tag tagInstance) {
         respond tagInstance
     }
 
+    /**
+     * Create new tag instance
+     * @return tag object
+     */
     def create() {
         respond new Tag(params)
     }
 
+    /**
+     * Save the instance of a tag
+     * @param tagInstance : object to save
+     * @return form of tag
+     */
     @Transactional
     def save(Tag tagInstance) {
         if (tagInstance == null) {
@@ -45,10 +67,20 @@ class TagController {
         }
     }
 
+    /**
+     * Edit tag instance
+     * @param tagInstance : object to edit
+     * @return tag
+     */
     def edit(Tag tagInstance) {
         respond tagInstance
     }
 
+    /**
+     * Update the tag instance
+     * @param tagInstance : object to update
+     * @return form for tag
+     */
     @Transactional
     def update(Tag tagInstance) {
         if (tagInstance == null) {
@@ -72,6 +104,11 @@ class TagController {
         }
     }
 
+    /**
+     * Delete a tag instance
+     * @param tagInstance : tag object
+     * @return confirmation message for deletion
+     */
     @Transactional
     def delete(Tag tagInstance) {
 
@@ -91,6 +128,9 @@ class TagController {
         }
     }
 
+    /**
+     * Error page, not found
+     */
     protected void notFound() {
         request.withFormat {
             form multipartForm {
