72714cece2b46ee47f4218e3b68aa6d8d6adfae8
smagras <magras.steve@gmail.com>
2015-11-01 21:39:29 +0100

    comments for statusservice #69

--- grails-app/services/bandstorm/service/StatusService.groovy
+++ grails-app/services/bandstorm/service/StatusService.groovy
@@ -4,9 +4,16 @@ import bandstorm.Status
 import bandstorm.User
 import grails.transaction.Transactional
 
+/**
+ * Services for status
+ */
 @Transactional
 class StatusService {
 
+    /**
+     * Get all status for connected user timeline
+     * @return list of status
+     */
     List<Status> getStatusForTimeline() {
         def criteria = Status.createCriteria()
         List<Status> res = criteria.list {
@@ -14,9 +21,14 @@ class StatusService {
         }
     }
 
-    List<Status> getStatusOfUser(User u) {
+    /**
+     * Get all status for selected user
+     * @param user : selected user
+     * @return list of status
+     */
+    List<Status> getStatusOfUser(User user) {
         List<Status> res = Status.createCriteria().list {
-            eq("author.id", u.id)
+            eq("author.id", user.id)
             order('dateCreated','desc')
         }
     }
